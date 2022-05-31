9273b79adbf6dbdb4781f0cca82b23b3913ac372
Bruno Moreira de Souza <bruno.m.souza@arcelormittal.com.br>
2022-05-31 18:12:45 -0300

    comitando resoluções dos exercicios

--- JS-Exercises/Brief Contents.md
+++
@@ -1,1 +0,0 @@
-This is my JavaScript exercises from the Eloquent JavaScript Book. Let's dive in.
--- JS-Exercises/fizzBuzz.js
+++
@@ -1,6 +0,0 @@
-let nums = 0;
-
-if ( nums = 1 && nums <= 100) {
-    return nums;
-}
-console.log(1); 
