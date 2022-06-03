d8b899dcb63d9220c084f85f6fad758ebcaee735
BRUNO MOREIRA <brunomoreiramg@hotmail.com>
2022-06-03 01:00:30 -0300

    Committing changes

--- Looping a Triangle/loopingTriangle.js
+++ Looping a Triangle/loopingTriangle.js
@@ -1,3 +1,16 @@
-for (let line = [' ', '#']; line.length < 16; line += ' ' + "#") {
-    console.log(line + '\n'); 
+let size = 8;
+
+let board = "";
+
+for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+        if ((x + y) % 2 == 0) {
+            board += " ";
+        } else {
+            board += "#";
+        }
+    }
+    board += "\n";
 }
+
+console.log(board);
